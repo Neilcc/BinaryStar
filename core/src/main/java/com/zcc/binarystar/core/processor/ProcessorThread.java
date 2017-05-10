e8ba44f0ca6173f03f76e64107054ced73822896
hengyun <hengyun@mogujie.com>
2017-05-10 16:12:58 +0800

    init

---
+++ core/src/main/java/com/zcc/binarystar/core/processor/ProcessorThread.java
@@ -0,0 +1,16 @@
+package com.zcc.binarystar.core.processor;
+
+import android.os.Handler;
+
+import com.zcc.binarystar.core.BinaryStarThread;
+
+/**
+ * Created by Hengyun on 09/05/2017.
+ */
+
+public class ProcessorThread extends BinaryStarThread {
+
+    public ProcessorThread(Handler mH) {
+        super(mH);
+    }
+}
