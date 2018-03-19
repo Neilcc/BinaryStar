5dc8686112c89632635b7535d9a419fc7e70649b
zhuchengcheng <zhuchengcheng@netease.corp.com>
2018-03-19 17:35:41 +0800

    feat: todo life circle

--- app/src/main/java/demo/binarystar/zcc/com/binarystar/TestContractView.java
+++ app/src/main/java/demo/binarystar/zcc/com/binarystar/TestContractView.java
@@ -1,10 +1,12 @@
 package demo.binarystar.zcc.com.binarystar;
 
+import com.zcc.binarystar.core.IBaseContractView;
+
 /**
  * Created by cc on 2018/3/16.
  */
 
-public interface TestContractView {
+public interface TestContractView extends IBaseContractView {
 
     public void operationOne(String param);
 
